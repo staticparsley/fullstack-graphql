fe1df488a8d594ae00eafa172e4aece451d47f9d
staticparsley <ruben.marin@fox.com>
2022-01-02 20:15:50 -0500

    added Schema and resolvers

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,24 @@
 
 module.exports = {
   Query: {
-    
+    pets(_, { input }, ctx) {
+      return ctx.models.Pet.findMany(input);
+    },
+    pet(_, { input }, ctx) {
+      return ctx.models.Pet.findOne(input);
+    },
   },
-  Mutation: {
-    
-  },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
-    }
-  },
-  User: {
-    
-  }
-}
+  // Mutation: {
+
+  // },
+  // Pet: {
+  //   img(pet) {
+  //     return pet.type === 'DOG'
+  //       ? 'https://placedog.net/300/300'
+  //       : 'http://placekitten.com/300/300';
+  //   },
+  // },
+  // User: {
+
+  // }
+};
